3b38bbb2b86b7597bd90b71fe86c37aebe7df680
yazyyyyy <yasharyanchopra2@gmail.com>
2020-03-02 19:38:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rotg/abt_activity.java
@@ -0,0 +1,14 @@
+package com.example.rotg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class abt_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abt_activity);
+    }
+}
---
+++ app/src/main/java/com/example/rotg/welcomeActivity.java
@@ -0,0 +1,105 @@
+package com.example.rotg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+
+
+public class welcomeActivity extends AppCompatActivity {
+    private Button button;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openqr();
+            }
+        });
+
+        button2=findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openreward();
+            }
+        });
+
+        button3=findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openloc();
+            }
+        });
+
+        button4=findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openacc();
+            }
+        });
+
+        button5=findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openfb();
+            }
+        });
+
+        button6=findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openabt();
+            }
+        });
+
+
+    }
+    public void openqr(){
+        Intent qrintent=new Intent(this,qr_activity.class);
+        startActivity(qrintent);
+    }
+
+    public void openreward(){
+        Intent rewardintent=new Intent(this,reward_activity.class);
+        startActivity(rewardintent);
+    }
+
+    public void openloc(){
+        Intent locintent=new Intent(this,loc_activity.class);
+        startActivity(locintent);
+    }
+
+    public void openacc(){
+        Intent accintent=new Intent(this,acc_activity.class);
+        startActivity(accintent);
+    }
+
+    public void openfb(){
+        Intent fbintent=new Intent(this,fb_activity.class);
+        startActivity(fbintent);
+    }
+
+    public void openabt(){
+        Intent abtintent=new Intent(this,abt_activity.class);
+        startActivity(abtintent);
+    }
+
+}
